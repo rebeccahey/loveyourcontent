822bca9e5609e86c1f65318760a77c6a8b512286
rebecca <rebeccahey1001@gmail.com>
2020-03-12 09:39:01 +0000

    add hotjar

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,14 @@ module.exports = {
       resolve: `gatsby-theme-codebushi`,
       options: {
         tailwindConfig: `tailwind.config.js`
-      }
-    }
-  ]
-};
+      },
+    },
+{
+  resolve: `gatsby-plugin-hotjar-tracking`,
+  options: {
+    id: '1726756',
+    sv: 'h._hjSettings={hjid:1726756,hjsv:6}'
+  },
+ },
+],
+}
